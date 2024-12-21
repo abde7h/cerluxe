9cd9d8d13b62a5d86f98b444ab85db0942f22c8c
Abdessamad belkhialat <abde.ripo@gmail.com>
2024-12-21 14:22:21 +0100

    fix viewport and delete localidades

--- src/app/viewport.ts
+++ src/app/viewport.ts
@@ -1,7 +1,11 @@
 import type { Viewport } from 'next'
 
-export const viewport: Viewport = {
+export const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 5,
-} 
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#000000' }
+  ]
+} satisfies Viewport 
